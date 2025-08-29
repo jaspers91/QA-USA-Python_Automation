bdf8a15b5be432161aa57cf89cc7238bc7a25fed
Stuart Jasper <jaspersbgsu@outlook.com>
2025-08-29 13:46:30 -0400

    Project 7 part one

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 # File with constants
+
+URBAN_ROUTES_URL = " https://cnt-815f17c1-0e3a-4a90-b792-a1206cef8871.containerhub.tripleten-services.com"
+ADDRESS_FROM = "East 2nd Street, 601"
+ADDRESS_TO = "1300 1st St"
+PHONE_NUMBER = "+1 123 123 12 12"
+CARD_NUMBER = "1234 5678 9100"
+CARD_CODE = "1111"
+MESSAGE_FOR_DRIVER = "stop at the juice bar, please"
+
+
+
